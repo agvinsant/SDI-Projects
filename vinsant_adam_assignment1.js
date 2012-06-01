f1ca7fd99f2334e4f33fedda9ef157a47d5ce9a9
Adam Vinsant <agvinsant@fullsail.edu>
2012-06-01 19:06:28 -0400

    Added first files to the repo.

---
+++ vinsant_adam_assignment1.js
@@ -0,0 +1,80 @@
+// The Javascripted Start To A Day In My Life
+// Author: Adam Vinsant
+// SDI 1205: Project 1
+// May 31, 2012
+
+var kidOneName = "Gavin",
+	kidTwoName = "GeAnna",
+	me = '"\Daddy"',
+	kidOneReady = 1
+	kidTwoReady = 2
+	readyForSchool = true
+;
+console.log('me: Good morning, ' + kidOneName + " and " + kidTwoName + "! " + "It's time to wake up!");
+console.log('kids: Good morning, ' + me + "!");
+console.log(kidOneName, ", are you getting up?");
+console.log(kidTwoName, ", are you getting up?");
+console.log('me: It\'s time to get ready for school.');
+
+// Thinking to myself.
+
+console.log(kidOneReady + " is ready, where is " + kidTwoName + "?");
+
+// Talking to the kids...
+
+console.log("How many of you are ready? ");
+console.log(kidOneReady +" kid is ready" 
+			+ " and " + kidTwoReady + " kids are ready");
+
+// Thinking again...
+
+console.log(kidOneName + ", has his stuff? " + kidOneName,":" + readyForSchool);
+console.log(kidTwoName + ", has her stuff? " + kidTwoName,":" + readyForSchool);
+
+if (readyForSchool === true) {
+	(kidOneReady + kidTwoReady) >= 3
+	console.log("Let's load up and head to school.")
+} else {
+	console.log("Hurry up kids and let's go!")
+};
+
+// I drive the kids to school.
+	
+console.log("Finally, kids are off to school.");
+
+// Back at home...
+
+console.log("Do I make breakfast or go back to sleep?");
+
+var backToSleep = "make sure to set the alarm.",
+	makeBreakfast = "time to make coffee and cereal and get started on school work.",
+	sleep = false
+	stayUP = true
+	shower = 3
+	uniform = 4
+	readyForWork = shower + uniform >= 7
+;
+console.log("If I go back to sleep, then " + backToSleep);
+console.log("If I stay up, then " + makeBreakfast);
+
+if (sleep === true) { 
+	console.log("Alarm sounds")
+};
+
+if (stayUP === true) {
+	console.log("Full belly and school work is done!")
+};
+
+
+console.log("Now it's time to get ready for work.");
+
+if (readyForWork === true) {
+	console.log("I am all ready for work, time for another day at CPK!!!")
+} else {
+	console.log("Oh shoot, I forgot my uniform shirt!!! I am going to be late!!!")
+};
+
+// But, I don't forget my shirt and I am on time as always to work!!! Successful morning!!!
+
+// The End! 
+	
