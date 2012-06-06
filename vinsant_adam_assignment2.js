56e6fd83c94a55f79fb9e8d65ca1bb74f59b49fc
Adam Vinsant <agvinsant@fullsail.edu>
2012-06-05 22:20:16 -0400

    start of my week 2 project

--- vinsant_adam_assignment2.js
+++ vinsant_adam_assignment2.js
@@ -1,1 +1,12 @@
-alert("JavaScript works!");
+// A Day In My Life...Part 2
+// Author: Adam Vinsant
+// SDI: 1206
+
+// Global Variables
+var family = ["Daddy", "Momma", "Gavin", "GeAnna"];
+var kidOneReady = 1;
+var kidTwoReady = 2;
+var ready = "Let's go!";
+var notReady = "HURRY UP!";
+
+
