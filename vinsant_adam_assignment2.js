0a9c8cde216f7873703b0e6874b6cae208818596
Adam Vinsant <agvinsant@fullsail.edu>
2012-06-06 00:44:21 -0400

    changed boolean code to make a function

--- vinsant_adam_assignment2.js
+++ vinsant_adam_assignment2.js
@@ -7,14 +7,16 @@ var family = ["Daddy", "Momma", "Gavin", "GeAnna"];
 var kidOneReady = 1;
 var kidTwoReady = 2;
 var ready = true;
-var notReady = false;
+var notReady = false
+;
 
 // Starting the day! (procedure)
 console.log("Alarm sounds and it's time to get up!");
 console.log("Time to get kids up and ready for school.");
 console.log("We can leave for school if "
             + family[2] + " and " + family[3]
-            + " have their stuff and ready.");
+            + " have their stuff and ready.")
+;
 
 if (ready === true) {
         (kidOneReady + kidTwoReady) <= 3
@@ -26,17 +28,25 @@ if (ready === true) {
 // Kids are off to school and now I am back home.
 
 // Do I go back to sleep or do I stay up and get the day going? (boolean function)
-var tired = false
-var awake = true 
-if ((tired || awake) && !(tired && awake)) {
-    console.log("I stay up and get my day started if I feel awake!")
-} else {
-    console.log("I am tired and will go back to sleep with " + family[1] + "!")
-};
+// t = tired, and a = awake
+var t = false,
+    a = true;
 
-// So I decide to stay up and get the day going
-// Time to make breakfast, but what to have? 
+var whatToDo = function () {
+    var stayAwake = "I stay up and get my day started!",
+        backToBed = "I am tired and will go back to sleep with " + family[1] + "!";   
+      if ((t || a) && !(t && a)) {
+        console.log(stayAwake)
+      } else { console.log(backToBed)
+      };
+    return ;
+}; 
 
+whatToDo();
 
+/* So I decide to stay up and get the day going
+ Time to make breakfast, but what to have? 
+ I want cereal, fruit, and a beverage. How many options do I have? (number function) 
+*/
     
     
