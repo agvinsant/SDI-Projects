ae26b8250a7ed7237853e5124b82d336e883e5f7
Adam Vinsant <agvinsant@fullsail.edu>
2012-06-06 09:16:22 -0400

    added fruit and bev choice functions

--- vinsant_adam_assignment2.js
+++ vinsant_adam_assignment2.js
@@ -46,7 +46,8 @@ whatToDo();
 
 /* So I decide to stay up and get the day going
  Time to make breakfast, but what to have? 
- I want cereal, I have 3 options, which to choose...(number function) 
+ I want cereal, I have 3 options, which to choose...(number function)
+ I also want fruit and beverage so I will show the functions for that too!
 */
 var cerealChoice = function () {
     var cerealTypes = 3;
@@ -60,7 +61,33 @@ var cerealChoice = function () {
 };
 
 cerealChoice();
+// This function shows that I have been through the choices one by one and found the one I wanted!
 
+var fruitChoice = function () {
+    var fruitTypes = 4
+    console.log("What kind of fruit do I want with my cereal? I have "
+                + fruitTypes + " to choose from." );
+        
+        while (fruitTypes > 1) {
+            console.log(fruitTypes + " kinds of fruit, which will go best with my cereal?...Na, next...");
+            fruitTypes--;
+        };
+        return(console.log("That will be good in my cereal!"));
+};
+
+fruitChoice(); // calls my fruitChoice function
+
+var bevChoice = function() {
+    var drinks= 3
+    console.log("Which drink will go best with my breakfast?");
+    
+    while (drinks > 1 ) {
+        console.log(drinks + " kinds of drinks to choose from...Na, next...");
+        drinks--;
+    };
+    return(console.log("I want to drink this with my breakfast!"))    
+};
+bevChoice(); // calls my bevChoice function
 
 
 
