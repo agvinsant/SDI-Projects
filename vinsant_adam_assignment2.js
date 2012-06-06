9185328c0c9681cb2c7bf42848fd60e24e6010e5
Adam Vinsant <agvinsant@fullsail.edu>
2012-06-05 22:50:23 -0400

    built the first procedure segment

--- vinsant_adam_assignment2.js
+++ vinsant_adam_assignment2.js
@@ -6,7 +6,23 @@
 var family = ["Daddy", "Momma", "Gavin", "GeAnna"];
 var kidOneReady = 1;
 var kidTwoReady = 2;
-var ready = "Let's go!";
-var notReady = "HURRY UP!";
+var ready = true;
+var notReady = false;
+
+// Starting the day! (procedure)
+console.log("Alarm sounds and it's time to get up!");
+console.log("Time to get kids up and ready for school.");
+console.log("We can leave for school if "
+            + family[2] + " and " + family[3]
+            + " have their stuff and ready.");
+
+if (ready === true) {
+        (kidOneReady + kidTwoReady) <= 3
+        console.log(family[0] + " says, " + "'Let's load up and head to school.'")
+} else {
+	console.log("Hurry up kids and let's go!")
+};
+
+// Kids are off to school and now I am back home. 
 
 
