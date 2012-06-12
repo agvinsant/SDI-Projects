1c185d81735d0b34e37ed3445ec7a7a4aaa7110c
Adam Vinsant <agvinsant@fullsail.edu>
2012-06-12 10:57:32 -0400

    Getting started with project 3, wrote first lines of code, json data, and edited html doc.

---
+++ jsoninfo.js
@@ -0,0 +1,59 @@
+// Adam Vinsant
+// SDI Project 3 1206
+// JSON Data
+
+var frontOfHouse = {
+     "servers": [
+     {
+        "name": "Adam",
+        "employeeId": "377001",
+        "title": "Shift Leader"
+     },
+     {
+        "name": "Eric",
+        "employeeId": "377002",
+        "title": "Server"
+     },
+     
+     {
+        "name":"Stephanie",
+        "employeeId": "377003",
+        "title": "Trainer"
+     },
+     {
+        "name": "Alex",
+        "employeeId": "377004",
+        "title": "Trainer"
+     },
+     {
+        "name": "Zak",
+        "employeeId": "377005",
+        "title": "Trainer"
+     },
+     {
+        "name": "Jeanine",
+        "employeeId": "377006",
+        "title": "Trainer"
+     },
+     {
+        "name": "Shelby",
+        "employeeId": "377007",
+        "title": "Server"
+     },
+     {
+        "name": "Mike",
+        "employeeId": "377008",
+        "title": "Server"
+     },
+     {
+        "name": "John",
+        "employeeId": "377009",
+        "title": "Server"
+     }
+     ]
+};
+
+        
+        
+        
+        
--- vinsant_adam_assignment3.js
+++ vinsant_adam_assignment3.js
@@ -1,1 +1,16 @@
-alert("JavaScript works!");
+// My Day At the Restaurant
+// Adam Vinsant
+// SDI 1206 Project 3
+
+console.log("Hello All, today we have 9 servers on the floor with one busser and one host.");
+console.log("Our servers for the day are: ");
+
+var serverOnDuty = function (frontOfHouse) {
+    for (var i = 0; i < frontOfHouse.servers.length; i++) {
+        var server = frontOfHouse.servers[i];
+        console.log("Name: " + server.name + ", Title: " + server.title);
+    };
+};
+
+serverOnDuty(frontOfHouse);
+
