4e69162245a87b112d7d0c79070438b15b6ff2a9
Adam Vinsant <agvinsant@fullsail.edu>
2012-06-13 11:43:38 -0400

    added several lines of code and a couple of functions.

--- vinsant_adam_assignment3.js
+++ vinsant_adam_assignment3.js
@@ -3,10 +3,10 @@
 // SDI 1206 Project 3
 
 // Start
-console.log("Hello All, today we have 9 servers on the floor with one busser and one host.");
-console.log("Our servers for the day are: ");
 
-// For Loop with Argument: Array from JSON Data
+console.log("Hello All, our servers for the day are: ");
+
+// For Loop with Argument: Object from JSON Data
 var serverOnDuty = function (frontOfHouse) {
     for (var i = 0; i < frontOfHouse.servers.length; i++) {
         var server = frontOfHouse.servers[i];
@@ -14,8 +14,19 @@ var serverOnDuty = function (frontOfHouse) {
     };
     
 };
+serverOnDuty(frontOfHouse); 
 
-serverOnDuty(frontOfHouse);
+//  Math with Number Return and Number Argument 
+var totalStaffOnDuty = function (numberStaff) {
+     var numberServers = 9,
+         numberBussers= 1,
+         numberHosts = 1;
+     var totalStaff = numberServers + numberBussers + numberHosts     
+        
+     return totalStaff;
+};
+var todayStaff = totalStaffOnDuty(11);
+console.log("We have a total of " + todayStaff + " staff members on the floor today.")
 
 // Global Variables
 var busser = "Carlos",
@@ -29,7 +40,7 @@ console.log("Your front of house manager is " + managerFOH + ", and the kitchen 
 console.log("This week's contest is selling desserts. You will get 1 point per dessert sold. If you do not sell a dessert in a day, you are out of the contest.")
 
 // Standard table greeting
-// Contains Global Variables, 
+// Contains Global Variables, local variables, while loop, nested conditional, array argument 
 var alcoholBeverage = ["beer", "wine", "coctail"],
         nonAlcoholBeverage = ["soda", "tea", "lemonade"],
         i = 3;
@@ -48,14 +59,18 @@ var tableGreeting = function (alcoholBeverage) {
     else {
         console.log(nonAlcoholOptions);
     };
+    return alcoholOptions;
 };
-tableGreeting(alcoholBeverage);
+beverageOrder = tableGreeting(alcoholBeverage);
+console.log(beverageOrder);
 
 
 
-// Conditional with return, string argument, number argument
 
-var dessertsSold = function (name, numberDessertsSold) {
+
+// Conditional with string return, string argument, number argument, boolean argument
+
+var dessertsSold = function (name, sellDessert, numberDessertsSold) {
     if (numberDessertsSold > 0) {
         var goodJob = name + ", great job selling keep it up!";
         console.log(goodJob);
@@ -64,15 +79,15 @@ var dessertsSold = function (name, numberDessertsSold) {
     }
     return goodJob; 
 };
-dessertsSold("Adam", 3);
-dessertsSold("Eric", 2);
-dessertsSold("Stephanie", 2);
-dessertsSold("Alex", 0);
-dessertsSold("Zak", 0)
-dessertsSold("Jeanine",1);
-dessertsSold("Shelby", 2);
-dessertsSold("Mike", 0);
-dessertsSold("John", 1);
+dessertsSold("Adam", true, 3);
+dessertsSold("Eric", true, 2);
+dessertsSold("Stephanie", true, 2);
+dessertsSold("Alex", false, 0);
+dessertsSold("Zak", false, 0)
+dessertsSold("Jeanine", true, 1);
+dessertsSold("Shelby", true, 2);
+dessertsSold("Mike", false, 0);
+dessertsSold("John", true, 1);
 
 
 
