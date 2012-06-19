d22dce949298a863e4a0da414d9f86a2226a871d
Adam Vinsant <agvinsant@fullsail.edu>
2012-06-18 23:35:46 -0400

    The start of Project 4. Starting with elapsed time function.

---
+++ library.js
@@ -0,0 +1,19 @@
+// Adam Vinsant
+// SDI 1206
+// Project 4
+// Library
+
+var adamLib = function () {
+   
+   // elapsed time function
+   var timeTillDue = function() {
+        var startDate = Date.now();
+        daysTillDue();
+        var dueDate = Date.now();
+        var timeTillDueDate = dueDate - startDate;
+        return timeTillDueDate;
+   };    
+            
+    
+};
+
---
+++ vinsant_adam_assignment4.js
@@ -0,0 +1,8 @@
+// Adam Vinsant
+// SDI 1206
+// Project 4
+
+// Calling elapsed time function
+var lib = new adamLib();
+console.log(lib.timeTillDue);
+
