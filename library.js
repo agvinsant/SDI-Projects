426782b4aa276ea894d2738f871be4cda22c2c2a
Adam Vinsant <agvinsant@fullsail.edu>
2012-06-19 22:56:36 -0400

    Fixed my mess...Fixed string to number function and added Time To Go function.

--- library.js
+++ library.js
@@ -8,11 +8,23 @@ var adamLib = function() {
   // string to number function
   var stringToNum = function(val) {
       var string = (val),
-          number = string * 1;
-      return {
-         "number": number
-      } 
+          number = string * 1
+      return number;
   };
    
+   // Time To Go function
+   var timeToGo = function(dateA, dateB) {
+     var start    = new Date(dateA),
+         end      = new Date(dateB),
+         milliseconds = 3600000,
+         timeLeft = (end - start)/milliseconds + " hours left."
+      return timeLeft;
+   };
    
+   
+   return {
+     "stringToNum": stringToNum,
+     "timeToGo": timeToGo
+      
+   };
 };
--- vinsant_adam_assignment4.js
+++ vinsant_adam_assignment4.js
@@ -2,8 +2,11 @@
 // SDI 1206
 // Project 4
 
-// Calling elapsed time function
+// variable for adamLib
 var lib = new adamLib();
 
-
+// Calling String to Number function
 console.log(lib.stringToNum("42"));
+
+// Calling Time to go function
+console.log(lib.timeToGo("June 19, 2012", "June 21, 2012"));
