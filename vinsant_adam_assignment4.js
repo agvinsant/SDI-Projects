c7b6994015414e65cfcb6cab2cb137c99cbab66a
Adam Vinsant <agvinsant@fullsail.edu>
2012-06-20 00:12:58 -0400

    Added some code to main .js file.

--- vinsant_adam_assignment4.js
+++ vinsant_adam_assignment4.js
@@ -6,10 +6,13 @@
 var lib = new adamLib();
 
 // Calling String to Number function
+console.log("Calling String to Number function:");
 console.log(lib.stringToNum("42"));
 
 // Calling Time to go function
+console.log("Calling Time to go function:");
 console.log(lib.timeToGo("June 19, 2012", "June 21, 2012"));
 
 // Calling the smallest number in an array that is greater than a given number function
+console.log("Calling the smallest number in an array that is greater than a given number function:");
 console.log(lib.findTheValue(5));
