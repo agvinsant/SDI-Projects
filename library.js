cbb0a239cabc81de1fac2361873da51b0f112279
Adam Vinsant <agvinsant@fullsail.edu>
2012-06-20 19:17:37 -0400

    Omitted one function and coded new Check Valid Email address and Check Valid URL functions... So far everything works. Using regExp now for some.

--- library.js
+++ library.js
@@ -4,14 +4,14 @@
 // Library
 
 var adamLib = function() {
-  
+ // Start Problem 1 
   // string to number function
   var stringToNum = function(val) {
       var string = (val),
           number = string * 1
       return number;
   };
-   
+   // Start Problem 2
    // Time To Go function
    // Created function to show both hours and days left. 
    var timeToGo = function(dateA, dateB) {  // This is also to show I learned how to work with arguments from Project 2.
@@ -19,12 +19,14 @@ var adamLib = function() {
          end      = new Date(dateB),
          millisecondsPerHour = 3600000,
          millisecondsPerDay = 86400000,
+         // Using math methods to find time left in hours and days
          hoursLeft = (end - start)/millisecondsPerHour + " hours left,",
          daysLeft  = (end - start)/millisecondsPerDay + " days left",
          timeLeft = hoursLeft + " or " + daysLeft;
       return timeLeft;
    };
    
+   // Start Problem 3
    // smallest number in an array that is greater than a given number
    var findTheValue = function (val) {
         var arr = [1,6,12,14];
@@ -40,26 +42,45 @@ var adamLib = function() {
       
    };
    
-   // Finding sum of numbers in an array funcion
-   var findTheNumbers = function () {
-      var myArray = ["a",1,"b",2,"c",3,"d",4],
-          sum = 0;
-      for (var i = 0; i < myArray.length; i++) {
-        if (!isNaN(parseInt(myArray[i]))) {
-            var totalValue = sum += myArray[i];
-        };
-      return;
-         
-   };
+   // Start Problem 4
+   // Validating email address function
+     // Messing around with some regExp.
      
-     //  
+   var checkValidEmail = function(emailToCheck) {
+      var  emailAddress = emailToCheck,
+           emailPattern = /^[a-zA-z0-9._-]+@[a-zA-z0-9._-]+\.[a-zA-Z]{2,4}$/   // regular expression pattern to test characters.
+      if (emailPattern.test(emailAddress)) {
+         console.log("The address you have entered is valid!");
+      } else {
+         console.log("The email address you have entered is invalid! Please Try again!");
+      };
    };
+   
+   // Start Problem 5
+   // Check URL function.
+   // Once again using regExp...
+   
+   var checkUrl = function(testUrl) {
+      var url = testUrl,
+          regExp = /^(http[s]?:\/\/){0,1}[a-zA-Z]{2,5}[\.]{0,1}/,  // regular expression pattern to test characters in URL.
+          isThisUrl = regExp.test(url);
+      // The following code will test the URL and return true if yes and false if no...
+      console.log("This is a valid URL: " + isThisUrl);
+      if (url.charAt(4) == "s") {
+         console.log("This is a https URL!");
+      };
+      // This will tell you if it is an https: or http: URL...
+      if (url.charAt(4) == ":") {
+         console.log("This is a http URL!")
+      };      
+   };
+   
    
    return {
      "stringToNum": stringToNum,
      "timeToGo": timeToGo,
      "findTheValue": findTheValue,
-     "findTheNumbers": findTheNumbers
-      
+     "checkValidEmail": checkValidEmail,
+     "checkUrl": checkUrl
    };
 };
--- vinsant_adam_assignment4.js
+++ vinsant_adam_assignment4.js
@@ -2,21 +2,33 @@
 // SDI 1206
 // Project 4
 
+// access to my library
 // variable for adamLib
 var lib = new adamLib();
 
+// Problem 1
 // Calling String to Number function
 console.log("Calling String to Number function:");
 console.log(lib.stringToNum("42"));
 
+// Problem 2
 // Calling Time to go function
 console.log("Calling Time to go function:");
 console.log(lib.timeToGo("June 19, 2012", "June 21, 2012"));
 
+// Probelm 3
 // Calling the smallest number in an array that is greater than a given number function
 console.log("Calling the smallest number in an array that is greater than a given number function:");
 console.log(lib.findTheValue(5));
 
-// Finding sum of numbers in an array funcion
-console.log("Finding sum of numbers in an array funcion:");
-console.log(lib.findTheNumbers());
+// Problem 4
+// Checking a valid email address function.
+// Checking my personal email address (agvinsant@gmail.com).
+console.log("Calling the valid email address function:")
+lib.checkValidEmail("agvinsant@gmail.com");
+
+// Problem 5
+// Checking a valid URL function
+// Checking with my music label's website: http://www.purelogicrecordings.com
+console.log("Calling the valid URL function:");
+lib.checkUrl("http://www.purelogicrecordings.com");
